40fe5d6d401da4efe137d6849770927b70512b3e
Brandon <branstainn@gmail.com>
2022-11-24 23:32:03 -0600

    first Commit

---
+++ src/Component/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import pokebola from '../assets/pokemon.png'
+import Pokedex from '../assets/camara.png';
+import pokedex from '../assets/pokedexPNG.png'
+import pokemon from '../assets/pikachu.png'
+
+
+const Header = () => {
+    return (
+        <div>
+            <div className='header'>
+                <img className='headerImg' src={pokedex} alt="" />
+                <div className='links'>
+                    <Link className='linksInd' to="/">Home
+                        <div>
+                            <img className='homeIcon' src={pokebola} alt="homeImg" />
+                        </div>
+                    </Link>
+                    <Link className='linksInd' to="/Pokedex">Pokedex
+                        <div>
+                            <img className='pokedexIcon' src={Pokedex} alt="pokedexImg" />
+                        </div>
+                    </Link>
+                    <Link className='linksInd' to="/pokemons/1">Pokemon Info
+                        <div>
+                            <img className='pokemonIcon' src={pokemon} alt="pokemonImg" />
+                        </div>
+                    </Link>
+                </div>
+                <div className='circuloHeader'>
+                    <div className='whiteC'></div>
+                    <div className='grayC'></div>
+                </div>
+            </div>
+            <div className='headerBlack'></div>
+        </div>
+    );
+};
+
+export default Header;
